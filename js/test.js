dac4397af444c758eb8ae26741f04bcd0d6112dc
Forrest22 <forforr@gmail.com>
2013-08-30 17:19:46 -0400

    Fixed some issues with generation of tiles

--- js/test.js
+++ js/test.js
@@ -1,6 +1,6 @@
 // The grid manages tiles using ids, which you can define. For our
 // examples we'll just use the tile number as the unique id.
-var TILE_IDS = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24 ];
+var TILE_IDS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24];
 
 // Grid
 $(function() {
@@ -12,21 +12,16 @@ $(function() {
         updateGrid = function(event, ui) {
 
             // update the set of tiles and redraw the grid
-            grid.updateTiles(TILE_IDS.slice(0, ui.value));
+            grid.updateTiles(TILE_IDS.slice(0, TILE_IDS.length-1));
             grid.redraw(true /* animate tile movements */);
-
-            // update the tile count label
-            $('#tileCount').text('(' + ui.value + ')');
         };
 
     grid.createTile = function(tileId) {
         var i = tileId;
         var tile = new Tiles.Tile(tileId);
-        $(tile).addClass("container portrait");
-        $.getJSON('https://gdata.youtube.com/feeds/api/videos?q=MrSuicideSheep&max-re‌​sults=5&v=2&alt=jsonc&orderby=published', function(ytdata) {
+        $.getJSON('https://gdata.youtube.com/feeds/api/videos?author=MrSuicideSheep&index-start='+(i+1)+'max-results=1&v=2&alt=jsonc&orderby=published', function(ytdata) {
             console.log(ytdata.data.items[i]);
-            var html = "<a href=\"http://img.youtube.com/vi/"+ ytdata.data.items[i].id +"/maxresdefault.jpg\" data-lightbox=\""+ ytdata.data.items[i].id +"\" title=\""+ ytdata.data.items[i].title +"\"><img class=\"thumbs\" src=\"http://img.youtube.com/vi/" + ytdata.data.items[i].id + "/maxresdefault.jpg\">";
-            console.log(html);
+            var html = "<a href=\"http://img.youtube.com/vi/"+ ytdata.data.items[i].id +"/maxresdefault.jpg\"><img class=\"thumbs\" src=\"http://img.youtube.com/vi/" + ytdata.data.items[i].id + "/maxresdefault.jpg\">";
             tile.$el.append(html);
             html =  "<h3 class=\"tile-title\"><a href=\"http://www.youtube.com/watch?v="+ ytdata.data.items[i].id +"\">" + ytdata.data.items[i].title + "<a></h3>";
             tile.$el.append(html);
@@ -34,17 +29,23 @@ $(function() {
         return tile;
     };
 
-    // use a jQuery slider to update the number of tiles
-    $('#image-tiles')
-        .slider({
-            min: 1,
-            max: 25,
-            step: 1,
-            create: updateGrid,
-            slide: updateGrid,
-            change: updateGrid
-        })
-        .slider('value', 8);
+    // testing out the use of a jQuery button
+    $( "button" )
+      .button({
+        create: updateGrid
+      })
+      .click(function(event) {
+        event.preventDefault();
+        var oldLength = TILE_IDS.length;
+        var newId;
+        for(var a = 0; a < 10; a++){
+            newId = TILE_IDS.length++;
+            TILE_IDS.push(newId);
+            console.log(newId);
+            grid.createTile(newId);
+        }
+        updateGrid(event);
+      });
 
     // wait until user finishes resizing the browser
     var debouncedResize = debounce(function() {
