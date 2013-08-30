2836e8dc31f2aca7c7ae99c22fa8881491fe1796
Forrest22 <forforr@gmail.com>
2013-08-30 18:01:06 -0400

    Fixed problems with loading more tiles.

--- js/test.js
+++ js/test.js
@@ -9,18 +9,20 @@ $(function() {
     // when either tile count or window size changes
     var el = document.getElementById('image-grid'),
         grid = new Tiles.Grid(el),
-        updateGrid = function(event, ui) {
+        updateGrid = function(event) {
 
             // update the set of tiles and redraw the grid
-            grid.updateTiles(TILE_IDS.slice(0, TILE_IDS.length-1));
+            grid.updateTiles(TILE_IDS.slice(0, TILE_IDS.length));
             grid.redraw(true /* animate tile movements */);
         };
 
     grid.createTile = function(tileId) {
-        var i = tileId;
+        var i = tileId%25;
         var tile = new Tiles.Tile(tileId);
-        $.getJSON('https://gdata.youtube.com/feeds/api/videos?author=MrSuicideSheep&index-start='+(i+1)+'max-results=1&v=2&alt=jsonc&orderby=published', function(ytdata) {
-            console.log(ytdata.data.items[i]);
+        console.log(tileId);
+        var startIndex = timesClicked*25 + 1;
+        $.getJSON('https://gdata.youtube.com/feeds/api/videos?author=MrSuicideSheep&start-index='+startIndex+'&max-results=25&v=2&alt=jsonc&orderby=published', function(ytdata) {
+            console.log(ytdata);
             var html = "<a href=\"http://img.youtube.com/vi/"+ ytdata.data.items[i].id +"/maxresdefault.jpg\"><img class=\"thumbs\" src=\"http://img.youtube.com/vi/" + ytdata.data.items[i].id + "/maxresdefault.jpg\">";
             tile.$el.append(html);
             html =  "<h3 class=\"tile-title\"><a href=\"http://www.youtube.com/watch?v="+ ytdata.data.items[i].id +"\">" + ytdata.data.items[i].title + "<a></h3>";
@@ -30,19 +32,21 @@ $(function() {
     };
 
     // testing out the use of a jQuery button
+    var timesClicked = 0;
     $( "button" )
       .button({
         create: updateGrid
       })
       .click(function(event) {
+        timesClicked++;
         event.preventDefault();
         var oldLength = TILE_IDS.length;
         var newId;
-        for(var a = 0; a < 10; a++){
-            newId = TILE_IDS.length++;
+        for(var a = 0; a < 25; a++){
+            newId = TILE_IDS.length;
             TILE_IDS.push(newId);
             console.log(newId);
-            grid.createTile(newId);
+            grid.addTiles(newId, false);
         }
         updateGrid(event);
       });
